a64be7a549aad6fbcee9ffbcdb229f79122fce5b
STEFANINI-DOM\asmandu <asmandu@stefanini.com>
2022-08-01 10:56:00 -0300

    Screenshot

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -9,15 +9,17 @@ Quando('logo com as credenciais {string} e {string}') do |email, senha|
   @login_page.button_signin
 
 end                                                                                                                                                          
-Então('verei a mensagem{string}') do |mensagem| 
+Então('verei a mensagem {string}') do |mensagem| 
   #O puts foi usado para investigar o que o metodo pega_titulo está retornando
   #puts @login_page.pega_titulo_pagina
   expect(@login_page.exibir_msg_myaccount).to have_content mensagem 
+  page.save_screenshot('log/screenshots/login_sucesso.png')
 end        
 Então('não consigo fazer o login e devo ver a mensagem {string}') do |mensagem| 
   file = YAML.load_file(File.join(Dir.pwd, 'features/support/fixtures/mensagem.yaml'))
   dados = file[mensagem]
   expect(@login_page.exibir_msg_erro).to have_content dados ["mensagem"]
+  page.save_screenshot('log/savescreenshot' + mensagem + '.png')
 end
 
 
